759f3c0a914636d5b359fe547be6efe761f98555
Ricardo <cryptosavesjesus@gmail.com>
2025-06-18 02:24:55 -0700

    Implemented validate_dna_sequence to check if DNA (A, G, C, T) are in the set and uppercase.

--- dna_sequencer.py
+++ dna_sequencer.py
@@ -5,7 +5,18 @@ from Bio.Seq import Seq
 # It has been deprecated and removed in favor of a more intuitive, object-oriented approach.
 # Instead of calling a separate function, you now call the .gc_content() method directly on 
 # a Seq object.
+from Bio.SeqUtils import gc_fraction
+from collections import Counter
 from Bio import SeqIO
+
+def validate_dna_sequence(sequence):
+    # check if sequence is DNA (only uppercase A, C, G, T)
+    valid = set('ACGT')
+    if set(str(sequence).upper()) != valid:
+    #condition below is same as condition above
+    # if not set(str(sequence).upper()).issubset(valid):
+        raise ValueError("Invalid DNA sequence. Only A, G, C, T allowed.")
+    return sequence.upper()
 
 def read_dna_sequence(file_path):
     with open(file_path, 'r') as file:
@@ -14,6 +25,7 @@ def read_dna_sequence(file_path):
 
 def main(file_path):
     sequence = read_dna_sequence(file_path)
+    validate_dna_sequence(sequence)
     print(f"Sequence: {sequence}")
 
 if __name__ == "__main__":
